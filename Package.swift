350321d6b5f2804edd727f2edf0569b25cf16959
KMI <michael.snow.net@gmail.com>
2024-11-25 17:45:47 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftWebSocket",
+    name: "JFWebSocket",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftWebSocket",
-            targets: ["SwiftWebSocket"]),
+            name: "JFWebSocket",
+            targets: ["JFWebSocket"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftWebSocket",
+            name: "JFWebSocket",
             dependencies: [], path: "Source"),
     ]
 )
